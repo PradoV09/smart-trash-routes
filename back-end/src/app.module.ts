3a5262f96841db3dbc5a651c45ccc790963402b2
Jose Luis Prado Valencia <joseluispradov046@gmail.com>
2025-10-07 11:19:32 -0500

    Configurando variables de entorno y puerto

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller'; // Acceso a las variavbles de entorno
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({  // Se encargará de traernos los datos del archivo de entorno .env
+      isGlobal: true, // hacer global el ConfigModule
+                      //Esto sirve para que no tengas que importar todo el rato el ConfigModule en otros módulos de Nest donde lo necesites usar.
+      })],
   controllers: [AppController],
   providers: [AppService],
 })
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configervice = app.get(ConfigService)
+  const PORT = configervice.get('PORT')
+  await app.listen(PORT ?? 3000);
+  console.log(`Servidor corriendo en el puerto: ${PORT ?? 3000}`);
 }
 bootstrap();
