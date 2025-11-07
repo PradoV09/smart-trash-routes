e6e2a2ba670bad3f1fd56a8d39d7a515003011f2
Jose Luis Prado Valencia <joseluispradov046@gmail.com>
2025-11-06 19:25:05 -0500

    Revert "Jose (#28)"
    This reverts commit 201d01de50e610c1a7184a2a88fdd65a8a38d4db.

--- back-end/src/app.service.ts
+++ back-end/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola mundo, mi aplicación NestJS si funciona!';
+    return 'Hello World!';
   }
 }
